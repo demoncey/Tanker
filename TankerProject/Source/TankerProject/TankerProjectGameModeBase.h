808e53030a7ef31bc85fb1136e2cf7e93d1ddfc0
demoncey <demoncey@gmail.com>
2018-07-23 21:33:22 +0200

    init

---
+++ TankerProject/Source/TankerProject/TankerProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankerProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKERPROJECT_API ATankerProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
